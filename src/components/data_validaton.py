73deb4916fb98e37d40e081cfd8103d00d76b57a
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-02-24 01:13:22 +0530

    Add simulation files in src/components

---
+++ src/components/data_validaton.py
@@ -0,0 +1,9 @@
+"""
+Simulation of real-life project, these files will also be present but for simplicity we are not creating this file in our project.
+
+data_validation.py
+
+This module is part of the 'src/components' package and is used for validating the input data for machine learning models. 
+It contains functions and classes that help in checking the quality, consistency, and completeness of the data. 
+The validation results can be used to clean the data, handle missing values, and ensure that the data meets the required standards for training models.
+"""
---
+++ src/components/model_evaluation.py
@@ -0,0 +1,9 @@
+"""
+Simulation of real-life project, these files will also be present but for simplicity we are not creating this file in our project.
+
+model_evaluation.py
+
+This module is part of the 'src/components' package and is used for evaluating machine learning models. 
+It contains functions and classes that help in assessing the performance of trained models using various metrics and techniques. 
+The evaluation results can be used to fine-tune models, compare different models, and ensure that the models meet the desired performance criteria.
+"""
