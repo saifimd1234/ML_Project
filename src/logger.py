3945d39e38cf0cec5f3260fd7fa5cf0fae49bf73
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-02-24 01:13:02 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -17,3 +17,26 @@ Usage:
 - Use the logger to record important events, errors, and other information that can help in debugging and monitoring 
     the application.
 """
+import logging  # Import the logging module to enable logging functionality
+import os  # Import the os module to interact with the operating system
+from datetime import datetime  # Import datetime to work with date and time
+
+# Create a log file name based on the current date and time
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+
+# Define the path where the log file will be stored
+logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+
+# Create the directory for logs if it doesn't exist
+os.makedirs(logs_path, exist_ok=True)
+
+# Define the full path for the log file
+LOGS_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure the logging system
+logging.basicConfig(
+    filename=LOGS_FILE_PATH,  # Set the log file path
+    format='[ %(asctime)s ]  %(lineno)d %(name)s - %(levelname)s: %(message)s',  # Define the log message format
+    level=logging.INFO,  # Set the logging level to INFO
+    filemode='a'  # Append to the log file if it exists
+)
